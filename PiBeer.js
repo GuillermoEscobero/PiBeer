ff9ddb6ac7040eefe5cd63fa225918aa0c581250
Raul Olmedo <100346073@alumnos.uc3m.es>
2017-02-23 23:17:32 +0100

    Mierda que me hizo hacer guille

--- PiBeer.js
+++ PiBeer.js
@@ -7,26 +7,20 @@ var roundedTempInside;
 //Setup Blynk (SSL)
 var blynk = new Blynk.Blynk(AUTH);
 
-//Sets the relay1 to 1
-gpio.setup(16, gpio.DIR_OUT, write);
-function write() {
-  gpio.write(16, true, function(err) {
-    console.log('relay set at high');
+if (Blynk.connected()) {
+  //28-051684c0d6ff built-in temp
+  //28-041692675aff ds18b20 temp
+  //Gets the temperature of the outside temp sensor and pushes it to blynk (in an interval of 1000 sec)
+  W1Temp.getSensor('28-041692675aff').then(function (sensor) {
+    setInterval(function() {
+    //Round the temperature to the nearest integer
+    roundedTempInside = Math.round(sensor.getTemperature()*10)/10;
+
+    //Display a log in the terminal
+    console.log('Temp on the inside:', roundedTempInside,'°C');
+
+    //Report it to server on virtual pin 0 (V0)
+    blynk.virtualWrite(0, roundedTempInside);
+    }, 1000);
   });
-}
-
-//28-051684c0d6ff built-in temp 
-//28-041692675aff ds18b20 temp
-//Gets the temperature of the outside temp sensor and pushes it to blynk (in an interval of 1000 sec)
-W1Temp.getSensor('28-041692675aff').then(function (sensor) {
-  setInterval(function() {
-  //Round the temperature to the nearest integer 
-  roundedTempInside = Math.round(sensor.getTemperature()*10)/10;
-
-  //Display a log in the terminal
-  console.log('Temp on the inside:', roundedTempInside,'°C');
-    
-  //Report it to server on virtual pin 0 (V0)
-  blynk.virtualWrite(0, roundedTempInside);
-  }, 1000);
-});
+};
