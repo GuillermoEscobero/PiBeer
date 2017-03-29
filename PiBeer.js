addf12060050e157dc271825a49f5639dee2ba7c
Raul Olmedo <100346073@alumnos.uc3m.es>
2017-03-29 12:42:26 +0200

    Changes

--- PiBeer.js
+++ PiBeer.js
@@ -1,5 +1,5 @@
 var W1Temp = require('w1temp');
-var Blynk = require ('blynk-library');
+var Blynk = require('blynk-library');
 var AUTH = 'aa46fedd9fcf42e4b7983939647db885';
 var roundedTempInside;
 var roundedTempOutside;
@@ -7,38 +7,30 @@ var roundedTempOutside;
 //Setup Blynk (SSL)
 var blynk = new Blynk.Blynk(AUTH);
 
-blynk.on('connect', function() {
+blynk.on('connect', function () {
 //28-051684c0d6ff built-in temp
 //28-041692675aff ds18b20 temp
 //Gets the temperature of the outside temp sensor and pushes it to blynk (in an interval of 1000 sec)
-  W1Temp.getSensor('28-041692675aff').then(function (sensor) {
-    setInterval(function() {
-      //Round the temperature to the nearest integer
-      roundedTempInside = Math.round(sensor.getTemperature()*10)/10;
 
-      //Display a log in the terminal
-      console.log('Temp on the inside:', roundedTempInside,'°C');
+    W1Temp.getSensor('28-041692675aff').then(function (sensor) {
+        console.log("Entro");
+    });
 
-      //Report it to server on virtual pin 0 (V0)
-      blynk.virtualWrite(0, roundedTempInside);
-    }, 10000);
-  });
+    W1Temp.getSensor('28-051684c0d6ff').then(function (sensor) {
+        setInterval(function () {
+            //Round the temperature to the nearest integer
+            roundedTempOutside = Math.round(sensor.getTemperature() * 10) / 10;
 
-  W1Temp.getSensor('28-051684c0d6ff').then(function (sensor) {
-    setInterval(function() {
-      //Round the temperature to the nearest integer
-      roundedTempOutside = Math.round(sensor.getTemperature()*10)/10;
+            //Display a log in the terminal
+            console.log('Temp on the outside:', roundedTempOutside, '°C');
 
-      //Display a log in the terminal
-      console.log('Temp on the outside:', roundedTempOutside,'°C');
+            //Report it to server on virtual pin 0 (V0)
+            blynk.virtualWrite(1, roundedTempOutside);
+        }, 10000);
+    });
 
-      //Report it to server on virtual pin 0 (V0)
-      blynk.virtualWrite(1, roundedTempOutside);
-    }, 10000);
-  });
-
-  blynk.on('disconnect', function() {
-    console.log("DISCONNECT");
-    blynk.connect();
-  });
+    blynk.on('disconnect', function () {
+        console.log("DISCONNECT");
+        blynk.connect();
+    });
 });
