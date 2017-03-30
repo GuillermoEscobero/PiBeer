960452c74a3b0d1ee299405613d6409823cf0681
Raul Olmedo <100346073@alumnos.uc3m.es>
2017-03-30 16:12:16 +0200

    Borrado todo lo de blink

--- PiBeer.js
+++ PiBeer.js
@@ -1,6 +1,9 @@
 var W1Temp = require('w1temp');
 var Blynk = require('blynk-library');
 var AUTH = 'aa46fedd9fcf42e4b7983939647db885';
+var v0 = new blynk.VirtualPin(0);
+var v1 = new blynk.VirtualPin(1);
+
 
 var blynk = new Blynk.Blynk(AUTH);
 
@@ -8,14 +11,18 @@ blynk.on('connect', function () {
 
     W1Temp.getSensor('28-041692675aff').then(function (sensor) {
         setInterval(function () {
-            console.log(sensor.getTemperature());
+            var temp = sensor.getTemperature()
+            console.log(temp);
+            v0.write(temp);
         }, 1000);
     });
 
 
     W1Temp.getSensor('28-051684c0d6ff').then(function (sensor) {
         setInterval(function () {
-            console.log(sensor.getTemperature());
+            var temp = sensor.getTemperature()
+            console.log(temp);
+            v1.write(temp);
         }, 1000);
     });
 });
