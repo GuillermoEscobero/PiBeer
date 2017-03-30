b7b54f2bc0172b4baf12bcba00e1ca3debc44df5
Raul Olmedo <100346073@alumnos.uc3m.es>
2017-03-30 15:28:48 +0200

    Commmit

--- PiBeer.js
+++ PiBeer.js
@@ -5,10 +5,10 @@ var AUTH = 'aa46fedd9fcf42e4b7983939647db885';
 //Setup Blynk (SSL)
 var blynk = new Blynk.Blynk(AUTH);
 
-blynk.on('connect', function () {
+blynk.on('connect', setInterval(function () {
 //28-051684c0d6ff built-in temp
 //28-041692675aff ds18b20 temp
-//Gets the temperature of the outside temp sensor and pushes it to blynk (in an interval of 1000 sec)
+
 
     W1Temp.getSensor('28-041692675aff').then(function (sensor) {
         console.log(sensor.getTemperature());
@@ -19,7 +19,7 @@ blynk.on('connect', function () {
     });
 
 
-});
+}, 10000));
 
 blynk.on('disconnect', function () {
     console.log("DISCONNECT");
