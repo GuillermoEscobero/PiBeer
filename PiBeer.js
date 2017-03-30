86e7f3ed3c98597c8747ed5c166f3207c214158c
Raul Olmedo <100346073@alumnos.uc3m.es>
2017-03-30 15:44:41 +0200

    Borrado todo lo de blink

--- PiBeer.js
+++ PiBeer.js
@@ -5,11 +5,11 @@ setInterval(
         W1Temp.getSensor('28-041692675aff').then(function (sensor) {
             console.log(sensor.getTemperature());
         });
-    }, 1000);
+    }, 10000);
 
 setInterval(
     function getTemperatureBuiltIn() {
         W1Temp.getSensor('28-051684c0d6ff').then(function (sensor) {
             console.log(sensor.getTemperature());
         });
-    }, 1000);
+    }, 10000);
