8321233d8afddd0c48541ea4c80fe66040f80145
Raul Olmedo <100346073@alumnos.uc3m.es>
2017-03-30 15:32:35 +0200

    Commmmmmit

--- PiBeer.js
+++ PiBeer.js
@@ -5,21 +5,21 @@ var AUTH = 'aa46fedd9fcf42e4b7983939647db885';
 //Setup Blynk (SSL)
 var blynk = new Blynk.Blynk(AUTH);
 
-blynk.on('connect', setInterval(function () {
+blynk.on('connect', function () {
 //28-051684c0d6ff built-in temp
 //28-041692675aff ds18b20 temp
 
 
-    W1Temp.getSensor('28-041692675aff').then(function (sensor) {
+    setInterval(W1Temp.getSensor('28-041692675aff').then(function (sensor) {
         console.log(sensor.getTemperature());
-    });
+    }), 1000);
 
     W1Temp.getSensor('28-051684c0d6ff').then(function (sensor) {
         console.log(sensor.getTemperature());
     });
 
 
-}, 10000));
+});
 
 blynk.on('disconnect', function () {
     console.log("DISCONNECT");
