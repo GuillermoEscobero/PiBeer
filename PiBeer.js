31a62b9b816016489fdcb7aa7bd0b7e4d031c792
Raul Olmedo <100346073@alumnos.uc3m.es>
2017-03-30 15:52:01 +0200

    Borrado todo lo de blink

--- PiBeer.js
+++ PiBeer.js
@@ -3,34 +3,12 @@ var W1Temp = require('w1temp');
 
 W1Temp.getSensor('28-041692675aff').then(function (sensor) {
     console.log(sensor.getTemperature());
-});
-W1Temp.getSensor('28-041692675aff').then(function (sensor) {
-    console.log(sensor.getTemperature());
-});
-W1Temp.getSensor('28-041692675aff').then(function (sensor) {
-    console.log(sensor.getTemperature());
-});
-W1Temp.getSensor('28-041692675aff').then(function (sensor) {
-    console.log(sensor.getTemperature());
-});
-W1Temp.getSensor('28-041692675aff').then(function (sensor) {
-    console.log(sensor.getTemperature());
-});
-W1Temp.getSensor('28-041692675aff').then(function (sensor) {
-    console.log(sensor.getTemperature());
-});
-W1Temp.getSensor('28-041692675aff').then(function (sensor) {
-    console.log(sensor.getTemperature());
-});
-W1Temp.getSensor('28-041692675aff').then(function (sensor) {
-    console.log(sensor.getTemperature());
-});
-W1Temp.getSensor('28-041692675aff').then(function (sensor) {
-    console.log(sensor.getTemperature());
+    sensor.on('change', function (temp) {
+        console.log('Temp', temp);
+    })
 });
 
-function getTemperatureBuiltIn() {
-    W1Temp.getSensor('28-051684c0d6ff').then(function (sensor) {
-        console.log(sensor.getTemperature());
-    });
-}
+
+W1Temp.getSensor('28-051684c0d6ff').then(function (sensor) {
+    console.log(sensor.getTemperature());
+});
